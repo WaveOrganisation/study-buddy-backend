a000cc04b9a17eceaa8a323d5e23422f935d1e0a
JuravskiLiron <116387515+Rror1K@users.noreply.github.com>
2024-11-17 00:07:39 +0200

    Docker fix

--- StudyBuddy.Domain/obj/Debug/net8.0/StudyBuddy.Domain.AssemblyInfo.cs
+++ StudyBuddy.Domain/obj/Debug/net8.0/StudyBuddy.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyBuddy.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e78e133c65c0faf32798d950e0a55b0cb28599")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b221016e8111674b6335997dadcaa2bedfb66ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyBuddy.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyBuddy.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
